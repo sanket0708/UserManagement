42b854d84cc2bbf9e24ceb2b177f5046feaf709b
Sanket Mudholkar <93979254+sanket0708@users.noreply.github.com>
2022-06-21 12:02:35 -0400

    Using the API's

--- server/services/render.js
+++ server/services/render.js
@@ -1,11 +1,22 @@
-exports.homeRoutes = (req,res) => {
-    res.render('index');
+const axios = require('axios');
+
+exports.homeRoutes = (req, res) => {
+    //Make a get req to the api users
+    axios.get('http://localhost:8000/api/users')
+        .then(function (response) {
+          
+            res.render('index', { users: response.data });
+        })
+        .catch(err => {
+            res.send(err);
+        })
+
 }
 
-exports.add_user = (req,res) => {
+exports.add_user = (req, res) => {
     res.render('add_user');
 }
 
-exports.update_user = (req,res) => {
+exports.update_user = (req, res) => {
     res.render('update_user');
 }
