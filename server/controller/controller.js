63fbec3ac37075a426ad60e0eeff3fe43ac0ad2d
Sanket Mudholkar <93979254+sanket0708@users.noreply.github.com>
2022-06-21 12:15:04 -0400

    Creating new user from browser

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -22,7 +22,8 @@ exports.create = (req, res) => {
     user
         .save(user)
         .then(data => {
-            res.send(data)
+           
+            res.redirect('/add-user')
         })
         .catch(err => {
             res.status(500).send({
