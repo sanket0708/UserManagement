128026b6318495f32fe1af5f1d5c05817e4150f6
Sanket Mudholkar <93979254+sanket0708@users.noreply.github.com>
2022-06-21 11:43:54 -0400

    Updating and Deleting the user from database

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -37,16 +37,67 @@ exports.create = (req, res) => {
 
 exports.find = (req, res) => {
 
+    Userdb.find()
+        .then(user => {
+            res.send(user)
+        })
+        .catch(err => {
+            res.status(500).send({ messgae: err.message || "Error occur while retrieving user information" })
+        })
+
+
 }
 
 //Update a new identified user by user id
 
 exports.update = (req, res) => {
 
+    if (!req.body) {
+        return res
+            .status(400)
+            .send({ message: "Data to update cannot be empty" })
+
+    }
+
+    const id = req.params.id;
+    Userdb.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+        .then(data => {
+            if (!data) {
+                res.status(404).send({ message: `Cannot update user with ${id}.Maybe user not found` })
+            }
+            else {
+                res.send(data)
+            }
+        })
+
+        .catch(err => {
+            res.status(500).send({ message: "Error update user information" })
+        })
+
+
 }
 
 //delete a user with specified user id
 
 exports.delete = (req, res) => {
+
+    const id = req.params.id;
+    Userdb.findByIdAndDelete(id)
+        .then(data => {
+            if (!data) {
+                res.status(404).send({ message: `Cannot delete with id ${id}.Maybe id is wrong` })
+            } else {
+                res.send({
+                    message: "User was deleted successfully"
+                })
+            }
+        })
+
+        .catch(err => {
+            res.status(500).send({
+                message: "Could not delete user with id =" + id
+            });
+        });
+
 
 }
